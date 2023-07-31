2a48382ec44159aae67036d3a09e3b64c1bf7796
Jean Santos <jean.ns24@hotmail.com>
2023-07-30 22:07:46 -0300

    Listando carrinho dinamicamente

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,17 +1,37 @@
 
+import {useContext} from 'react'
+import {CartContext} from '../../contexts/CartContext'
+import { Link } from 'react-router-dom';
+
 export function Cart(){
+
+  const {cart} = useContext(CartContext);
+
     return(
       <div className="w-full max-w-7xl mx-auto">
         <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
-  
-        <section className="flex items-center justify-between border-b-2 border-gray-300">
+
+        {cart.length === 0 && (
+          <div className='flex flex-col items-center justify-center'>
+          <p className='font-medium'>Ops! seu carrinho está vazio...</p>
+          <Link 
+          to="/"
+          className='bg-slate-600 my-3 p=1 px-3 text-white font-medium rounded'
+          >
+            Acessar produtos
+          </Link>
+        </div> 
+        )}
+
+        {cart.map((item) => (
+          <section key={item.id} className="flex items-center justify-between border-b-2 border-gray-300">
           <img
-            src="https://images.tcdn.com.br/img/img_prod/167552/fone_de_ouvido_apple_airpods_pro_mwp22be_a_13305_1_49b712f1e0c3353c688e35bd6034170e.jpg"
-            alt="Logo produto" 
+            src={item.cover}
+            alt={item.title}
             className="w-28"
           />
   
-          <strong>Preço: R$1.000</strong>
+          <strong>{item.price}</strong>
   
           <div className="flex items-center justify-center gap-3">
             <button
@@ -19,9 +39,7 @@ export function Cart(){
             >
               -
             </button>
-  
             1
-  
             <button
             className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center"
             >
@@ -29,13 +47,18 @@ export function Cart(){
             </button>
           </div>
   
-  
           <strong className="float-right">
-            SubTotal: R$1.000
+
+            SubTotal: {item.total.toLocaleString("pt-BR",{
+              style:"currency",
+              currency:"BRL"
+            })}
           </strong>  
         </section>
-  
-        <p className="font-bold mt-4">Total: R$1.000</p>
+        ))}
+        {cart.length != 0 && (
+          <p className="font-bold mt-4">Total: R$1.000</p>
+        )}
       </div>
     )
   }
