389bee2749b5ac2ad9d0a9bdf67ab73717ee0bc3
Jean Santos <jean.ns24@hotmail.com>
2023-08-02 22:35:48 -0300

    Criando a pagina detalhes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Home } from './pages/home'
 import { Cart } from './pages/cart'
 import { Layout } from './components/layout'
+import {ProductDetail} from './pages/detail'
+
 
 const router = createBrowserRouter([
   {
@@ -14,6 +16,10 @@ const router = createBrowserRouter([
       {
         path:"/cart",
         element:<Cart/>
+      },
+      {
+        path:"/product/:id",
+        element:<ProductDetail/>
       }
     ]
   }
--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,15 @@ import {RouterProvider} from 'react-router-dom'
 import './index.css'
 import './index.css'
 import CartProvider from './contexts/CartContext'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CartProvider>
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+    />
       <RouterProvider router={router}/>
     </CartProvider>
   </React.StrictMode>,
